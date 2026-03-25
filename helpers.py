5810d5507d12dc34ee10a6589d5d6c30dd56bd66
Fabian Ternis <f.ternis@xpsystems.eu>
2026-03-25 19:05:15 +0100

    stuff ...

---
+++ helpers.py
@@ -0,0 +1,6 @@
+def empty_line(count = 1):
+    for i in range(count):
+        print()
+
+def emptyLine(count):
+    empty_line(count)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print ('Hello World!')
 
 import os
+import helpers as h
 
 if os.path.exists('tmp'):
     os.rmdir('tmp')
@@ -8,3 +9,14 @@ if os.path.exists('tmp'):
 
 os.mkdir('tmp')
 print('tmp-dir created')
+h.empty_line()
+
+in1 = input("Do you want to use a filepath? [Y/n]")
+
+match in1.lower():
+    case "y":
+        pass
+    case "n":
+        print ("OK, (To be implemented)")
+    case _:
+        print ("Re-input (to be implemented)")
